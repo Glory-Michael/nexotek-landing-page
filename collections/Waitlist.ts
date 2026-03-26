d374f84c519f948397c28248a6fb73a992725fdf
Michael-Xu <31372042+Glory-Michael@users.noreply.github.com>
2026-03-26 19:57:07 -0400

    Fix name conflict with db with payload & supabase

--- collections/Waitlist.ts
+++ collections/Waitlist.ts
@@ -2,6 +2,7 @@ import type { CollectionConfig } from 'payload';
 
 export const Waitlist: CollectionConfig = {
   slug: 'waitlist',
+  dbName: 'payload_waitlist',
   admin: {
     useAsTitle: 'email',
     defaultColumns: ['email', 'status', 'createdAt'],
